2fb68efea218dfd3f10b71ac3b9b44a6b49b5746
francisco <f.forte@campus.fct.unl.pt>
2022-11-21 23:08:18 +0000

    add search results page

---
+++ javascript/search.js
@@ -0,0 +1,57 @@
+function genSearch() {
+    // get search body
+    const searchBody = document.getElementById('search-results')
+
+    // query params fetcher
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(prop),
+    });
+    
+    let searchResult = params.search
+    if (searchResult == undefined) {
+        searchBody.insertAdjacentHTML('beforeend', '<h3 class="text-white ms-1 mt-5">Algo inesperado aconteceu!?</h3>')
+        return
+    }
+    searchResult = searchResult.toUpperCase()
+
+    fetch(location.href.split('html')[0] + 'data/content.json')
+        .then((response) => response.json())
+        .then((json) => {
+            function contents() {
+                let items = ''
+
+                for (let categoryName in json) {
+                    for (let content of json[categoryName]) {
+                        if (content.name.toUpperCase().includes(searchResult)) {
+                            items += 
+                            `
+                            <div class="col">
+                                <div class="card border-0 bg-dark">
+                                    <div class="card-img-bg-color">
+                                        <a href="#">
+                                            <img class="img-fluid rounded-5 h-100 w-100 card-img-top" alt="" src="../images/content/${content.image}">
+                                        </a>
+                                    </div>
+                                    <p class="ms-4 mt-2 text-white" style="margin-bottom: 0px;"><b>${content.name}</b></p>
+                                </div>  
+                            </div>
+                            `
+                        }
+                    }
+                }
+                return items
+            }
+
+            let results, its = contents()
+            if (its != '') {
+                results = `<div class="row row-cols-5 ms-1 mt-5">${its}</div>`
+            }
+            else {
+                results = '<h3 class="text-white ms-1 mt-5">Sem resultados!</h3>'
+            }
+
+            searchBody.insertAdjacentHTML('beforeend', results)
+        });
+}
+
+genSearch()
