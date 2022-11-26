343f07dec94b5b0030505925dfc671cc56680be3
francisco <f.forte@campus.fct.unl.pt>
2022-11-26 19:18:44 +0000

    fgfg

--- javascript/movie-doc.js
+++ javascript/movie-doc.js
@@ -14,6 +14,7 @@ function genMovieDoc() {
         .then((json) => {
             let content = json[name]
             
+            document.getElementById('myear').innerHTML = content.ano;
             document.getElementById('mduration').innerHTML = content.duration;
             document.getElementById('rating').innerHTML = content.evaluation;
             document.getElementById('mprofile').src = "../images/content/" + content.category + "/" + content.profile;
